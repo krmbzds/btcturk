e17bcc0c8966d28f1cb456fec24c57faa9b418b8
Kerem Bozdas <krmbzds.github@gmail.com>
2014-06-08 05:34:33 +0300

    Add authentication and private API functions

--- lib/btcturk.rb
+++ lib/btcturk.rb
@@ -1,5 +1,9 @@
+require 'uri'
 require 'json'
-require 'open-uri'
+require 'yaml'
+require 'base64'
+require 'openssl'
+require 'net/http'
 require 'btcturk/version'
 
 module BTCTurk
@@ -7,24 +11,109 @@ module BTCTurk
   BASE_URL = 'https://www.btcturk.com/api/'
 
   def self.ticker
-    fetch 'ticker'
+    get 'ticker'
   end
 
   def self.orderbook
-    fetch 'orderbook'
+    get 'orderbook'
   end
 
   def self.trades
-    fetch 'trades'
+    get 'trades'
   end
 
   def self.trades_since(id)
-    fetch "trades?sinceid=#{id}"
-
+    get "trades?sinceid=#{id}"
   end
 
-  def self.fetch(request)
-    JSON.parse(open(BASE_URL + request).read)
+  def self.balance
+    get 'balance'
+  end
+
+  def self.orders
+    get 'openOrders'
+  end
+
+  # Check lines [61-64]
+  def self.transactions(offset = '0', limit = '100', sort = 'desc')
+    post 'userTransactions', offset, limit, sort
+  end
+
+  def self.get(action)
+    @settings = get_config
+
+    uri = URI.parse(BASE_URL + action)
+    timestamp = Time.now.to_i.to_s
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    request['X-PCK'] = @settings[:api_key]
+    request['X-Stamp'] = timestamp
+    request['X-Signature'] = get_signature(timestamp)
+
+    response = http.request(request)
+    JSON.parse(response.body)
+  end
+
+  # User Transactions do not work at the moment
+  # Not sure if it's my code or the API to blame
+  # I'll try to solve it ASAP. In the meanwhile,
+  # Please do not hesitate to send pull requests!
+
+  def self.post(action, offset, limit, sort)
+    @settings = get_config
+    uri = URI.parse(BASE_URL + action)
+    timestamp = Time.now.to_i.to_s
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+
+    request.set_form_data({'offset' => offset, 'limit' => limit, 'sort' => sort})
+    # request.set_body_internal({'offset' => offset, 'limit' => limit, 'sort' => sort})
+    # request.body = 'offset=0&limit=100&sort=desc'
+
+    request['X-PCK'] = @settings[:api_key]
+    request['X-Stamp'] = timestamp
+    request['X-Signature'] = get_signature(timestamp)
+
+    response = http.request(request)
+    response.body
+  end
+
+  def self.get_signature(timestamp)
+    message = @settings[:api_key] + timestamp
+    private_key = Base64.decode64(@settings[:private_key]).strip
+    digest = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), private_key, message)
+    Base64.encode64(digest).strip
+  end
+
+  def self.set_config
+    settings = {}
+    printf('Please enter your API key: ')
+    settings[:api_key] = gets.chomp
+    printf('Please enter your Private key: ')
+    settings[:private_key] = gets.chomp
+    printf("(Saved under #{Dir.home}/.btcturk ...)\n\n")
+
+    File.open("#{Dir.home}/.btcturk", 'w') do |file|
+      file.write settings.to_yaml
+    end
+
+    get_config
+  end
+
+  def self.get_config
+    file = "#{Dir.home}/.btcturk"
+    if File.exist? file
+      YAML::load_file file
+    else
+      set_config
+    end
   end
 
 end
